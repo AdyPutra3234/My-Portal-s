50fb6efd60cb391bd7c573ace99e761ecb78b920
AdyPutra3234 <romadona.xtl4@gmail.com>
2026-05-07 21:05:17 +0900

    edit/cleaning code

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,5 +1,3 @@
-@props(['article'])
-
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
         @foreach ($articles as $article)
                 <x-article-card :article="$article"></x-article-card>     
